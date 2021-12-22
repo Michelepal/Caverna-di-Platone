879838b587a0f20f18481b52231d72207a41c103
Michelepal <mikaelquasimodus@gmail.com>
2021-12-22 19:10:09 +0100

    scrittura logica component vitaPlatone

--- src/app/vita-platone/vita-platone.component.ts
+++ src/app/vita-platone/vita-platone.component.ts
@@ -7,15 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VitaPlatoneComponent implements OnInit {
  
-  vita:number = 0;
+  vita: number = 0;
 
   constructor() { 
   }
 
   ngOnInit(): void {
+ 
 
-    
 
   }
+
+scelta(x: number): void {
+ 
+  if (x == 1) {
+     this.vita = 1;  
+  } else if (x == 2) {
+    this.vita = 2;
+  } else if (x == 3) {
+    this.vita = 3;
+  } else if (x == 4) {
+    this.vita = 4;
+  } else if (x == 5) {
+    this.vita = 5;
+  } else if (x ==6) {
+    this.vita = 6;
+  } else if (x == 7) {
+    this.vita = 7;
+  }
+
+}
 
 }
